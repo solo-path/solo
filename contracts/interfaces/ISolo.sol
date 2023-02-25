c1794c6451a193120e94b25ea5c9098a707991b5
Vladimir Stemkovski <stemkovs@yahoo.com>
2023-02-25 17:50:02 -0500

    ISolo interface with the main functions

---
+++ contracts/interfaces/ISolo.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.14;
+
+interface ISolo {
+
+    function firstDeposit(
+        uint256 amountDeposit,
+        uint256 amountQuote,
+        uint256 price
+    ) external;
+
+    function deposit(
+        uint256 amountDeposit,
+        address to
+    ) external returns (uint256 lpTokens);
+
+    function withdraw(
+        uint256 lpAmount, 
+        address to
+    ) external returns (uint256 amountDeposit, uint256 amountQuote);
+
+    function swapExactInput(uint256 amount0, uint256 amount1) external 
+        returns (uint256 output0, uint256 output1);
+
+}
